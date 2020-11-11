7f4427cfb73ff465dca1d0f340f38955462fbc92
driouecheMed <drioueche.med@gmail.com>
2020-11-11 21:41:58 +0100

    Generalize HPF_RGB_img script

--- Matlab_algorithm/HPF_RGB_img.m
+++ Matlab_algorithm/HPF_RGB_img.m
@@ -10,22 +10,22 @@ img = imread ('football.jpg');
 % Compute Laplacian Conv
 % kernel = [0 1 0; 1 -4 1; 0 1 0];
 % Red Channel
-for i =1:254
-    for j = 1:318
+for i = 1 : (width - 2)
+    for j = 1 : (height - 2)
             R(i, j) = img(i, j+1, 1) + img(i+1, j, 1) - 4*img(i+1, j+1, 1) + img(i+1, j+2, 1) + img(i+2, j+1, 1);
     end
 end
 
 % Green Channel
-for i =1:254
-     for j = 1:318
+for i = 1 : (width - 2)
+    for j = 1 : (height - 2)
             G(i, j) = img(i, j+1, 2) + img(i+1, j, 2) - 4*img(i+1, j+1, 2) + img(i+1, j+2, 2) + img(i+2, j+1, 2);
      end
 end
 
 % Bleu Channel
-for i =1:254
-     for j = 1:318
+for i = 1 : (width - 2)
+    for j = 1 : (height - 2)
             B(i, j) = img(i, j+1, 3) + img(i+1, j, 3) - 4*img(i+1, j+1, 3) + img(i+1, j+2, 3) + img(i+2, j+1, 3);
      end
 end
