495acb642048630c09dadf532fd52021e14aed86
driouecheMed <drioueche.med@gmail.com>
2020-11-19 16:42:18 +0100

    Add sharped Image

--- Matlab_algorithm/HPF_RGB_img.m
+++ Matlab_algorithm/HPF_RGB_img.m
@@ -3,8 +3,8 @@ clear, close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Read RGB Image
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% img = imread ('vegetables.jpg');
-img = imread('RGB.png');
+img = imread ('test.jpg');
+% img = imread('RGB.png');
 
 [width, height, channels] = size(img);
 img = double(img);
@@ -30,50 +30,66 @@ p_img_B(2 : width+1, 2 : height+1) = double(img(:,:,3));
 % kernel = [-1 -1 -1; -1 8 -1; -1 -1 -1];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Red Channel
-R = uint8(zeros([width, height]));
+HPF_Red = uint8(zeros([width, height]));
+sharped_Red = uint8(zeros([width, height]));
 for i = 2 : (width + 1)
     for j = 2 : (height + 1)
-        R(i-1, j-1) = -p_img_R(i-1, j-1) - p_img_R(i-1, j) - p_img_R(i-1, j+1) - p_img_R(i, j-1) + (8 * p_img_R(i, j)) - p_img_R(i, j+1) - p_img_R(i+1, j-1) - p_img_R(i+1, j) - p_img_R(i+1, j+1);
+        HPF_Red(i-1, j-1) = -p_img_R(i-1, j-1) - p_img_R(i-1, j) - p_img_R(i-1, j+1) - p_img_R(i, j-1) + (8 * p_img_R(i, j)) - p_img_R(i, j+1) - p_img_R(i+1, j-1) - p_img_R(i+1, j) - p_img_R(i+1, j+1);
+        sharped_Red(i-1, j-1) = p_img_R(i, j) - HPF_Red(i-1, j-1);
     end
 end
 
 % Green Channel
-G = uint8(zeros([width, height]));
+HPF_Green = uint8(zeros([width, height]));
+sharped_Green = uint8(zeros([width, height]));
 for i = 2 : (width + 1)
     for j = 2 : (height + 1)
-        G(i-1, j-1) = -p_img_G(i-1, j-1) - p_img_G(i-1, j) - p_img_G(i-1, j+1) - p_img_G(i, j-1) + (8 * p_img_G(i, j)) - p_img_G(i, j+1) - p_img_G(i+1, j-1) - p_img_G(i+1, j) - p_img_G(i+1, j+1);
+        HPF_Green(i-1, j-1) = -p_img_G(i-1, j-1) - p_img_G(i-1, j) - p_img_G(i-1, j+1) - p_img_G(i, j-1) + (8 * p_img_G(i, j)) - p_img_G(i, j+1) - p_img_G(i+1, j-1) - p_img_G(i+1, j) - p_img_G(i+1, j+1);
+        sharped_Green(i-1, j-1) = p_img_G(i, j) - HPF_Green(i-1, j-1);
      end
 end
 
 % Bleu Channel
-B = uint8(zeros([width, height]));
+HPF_Blue = uint8(zeros([width, height]));
+sharped_Blue = uint8(zeros([width, height]));
 for i = 2 : (width + 1)
     for j = 2 : (height + 1)
-        B(i-1, j-1) = -p_img_B(i-1, j-1) - p_img_B(i-1, j) - p_img_B(i-1, j+1) - p_img_B(i, j-1) + (8 * p_img_B(i, j)) - p_img_B(i, j+1) - p_img_B(i+1, j-1) - p_img_B(i+1, j) - p_img_B(i+1, j+1);
+        HPF_Blue(i-1, j-1) = -p_img_B(i-1, j-1) - p_img_B(i-1, j) - p_img_B(i-1, j+1) - p_img_B(i, j-1) + (8 * p_img_B(i, j)) - p_img_B(i, j+1) - p_img_B(i+1, j-1) - p_img_B(i+1, j) - p_img_B(i+1, j+1);
+        sharped_Blue(i-1, j-1) = p_img_B(i, j) - HPF_Blue(i-1, j-1);
     end
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Displaying
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-output_img = cat(channels, R, G, B);
-% size(output_img)
+HPF_result_img = cat(channels, HPF_Red, HPF_Green, HPF_Blue);
+% size(HPF_result_img)
+
+sharped_result_img = cat(channels, sharped_Red, sharped_Green, sharped_Blue);
+% size(HPF_result_img)
+
 subplot(3,3,2);
+img = uint8(img);
 imshow(img);
-title('HPF RGB Result');
+title('Original Image');
 
 subplot(3,3,4);
-imshow(R);
+imshow(HPF_Red);
 title('HPF Red Layer Result');
 
 subplot(3,3,5);
-imshow(G);
+imshow(HPF_Green);
 title('HPF Green Layer Result');
 
 subplot(3,3,6);
-imshow(B);
+imshow(HPF_Blue);
 title('HPF Bleu Layer Result');
 
 subplot(3,3,7);
-imshow(output_img);
+imshow(HPF_result_img);
 title('HPF RGB Result');
+
+subplot(3,3,9);
+imshow(sharped_result_img);
+title('Sharped RGB Image');
+
